cfed643d9ced86aa83142b37aaccb061b77e47de
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2020-10-02 11:58:21 +1000

    minor updates

--- vanilla-js/src/basics/data-type_number.js
+++ vanilla-js/src/basics/data-type_number.js
@@ -16,8 +16,11 @@ function this_line(stack_item_index = 2, show_filepath = true) {
     return message;
 }
 
+var dd_enabled = true;
 const dd = function (message, stack_item_index = 3, show_filepath = true) {
-
+    if (dd_enabled === false) {
+        return;
+    }
     console.log(message);
     console.log('|_ ' + this_line(stack_item_index, show_filepath));
 
@@ -28,56 +31,246 @@ const dd = function (message, stack_item_index = 3, show_filepath = true) {
 * ************************************************************
  * */
 
-let my_number = 148833;
-let my_number_object = new Number(148833);
 /*
 * ************************************************************
 *
 * */
 
-dd(typeof my_number); // prints "number"
-
-
-dd(my_number_object); // prints "[Number: 148833]"
-dd(my_number_object.valueOf()); // prints "148833"
-dd(my_number_object.constructor.name) // prints "Number"
-/*
-* ************************************************************
-*
-* */
-
-dd(my_number_object); // prints ""
 
 /*
 * ************************************************************
-*
+* Number static properties
 * */
+
+
+// Returns max allowed JS number
+dd(Number.MAX_VALUE);
+
+// Returns min allowed JS number
+dd(Number.MIN_VALUE);
+
+// Returns max safe JS number : 9007199254740991
+dd(Number.MAX_SAFE_INTEGER);
+
+// Returns min safe JS number : -9007199254740991
+dd(Number.MIN_SAFE_INTEGER);
+
+// Returns 'Infinity'
+dd(Number.POSITIVE_INFINITY);
+
+// Returns '-Infinity'
+dd(Number.NEGATIVE_INFINITY);
+
 
 /*
 * ************************************************************
-*
+* Numbers formatting
 * */
+// Returns '"1.5e+5"' , rounded & formatted to 2 decimal points as presented in exponential notation as a string.
+dd(Number(148833).toExponential(2));
+
+// Returns '"1.4883300000e+5"' , rounded & formatted to 10 decimal points as presented in exponential notation as a string.
+dd(Number(148833).toExponential(10));
+
+// print '150000' as a number after converting exponential notation string to a number.
+dd(Number(Number(148833).toExponential(1)));
+
+// Returns '"1.49e+5"', after formatting the number to 3 characters (including the decimal points), presented in exponential notation if needed. zero padded if needed.
+dd(Number(148833).toPrecision(3));
+
+// Returns '"148833.00000000000000"', after formatting the number to 3 characters (including the decimal points).
+// Zero padded if needed.
+dd(Number(148833).toPrecision(20));
+
+// Returns '"1.49e+5"', fixed to 3 decimal points, zero padded if needed.
+dd(Number(148833).toFixed(3));
+
+
+// Returns '"47.9"', after formatting the number to 3 characters (including the decimal points), presented in exponential notation if needed.
+dd(Number(47.8635).toPrecision(3));
+
+// Returns '"47.863500000000001933"', after formatting the number to 3 characters (including the decimal points). The extra numbers are due to precision issues with JS Math & binary code.
+dd(Number(47.8635).toPrecision(20));
+
+// Returns '"47.9"', fixed to 3 decimal points. zero padded if needed.
+dd(Number(47.8635).toFixed(3));
+
 
 /*
 * ************************************************************
-*
+* Converting strings to numbers.
 * */
+
+dd(Number(148833));
+// print '148833'.
+
+dd(Number('148833'));
+// print '148833'.
+
+dd(Number('7e2'));
+// print '700'.
+
+
 /*
 * ************************************************************
 * Converting number to other data types.
 * */
-dd(my_number.toString());
-dd(Number(my_number)); // prints '148833'
-dd(parseInt(my_number)); // prints '148833'
-dd(parseFloat(my_number)); // prints '148833'
-dd(String(my_number)); // prints '"148833"'
-dd(Boolean(my_number)); // prints 'true'
-dd(Array(my_number)); // prints '[ <148833 empty items> ]', which is an array with many empty items counting up to 148833 !
+
+dd(Number(148833).toString());
+// print '"148833"'.
+
+dd(Number(148833).toLocaleString('ar-SA'));
+// print '"١٤٨٬٨٣٣"'.
+
+dd(Number(Number(148833)));
+// Returns '148833'
+
+dd(parseInt(Number(148833)));
+// Returns '148833'
+
+dd(parseFloat(Number(148833)));
+// Returns '148833'
+
+dd(String(Number(148833)));
+// Returns '"148833"'
+
+dd(Boolean(Number(148833)));
+// Returns 'true'
+
+dd(Array(Number(13)));
+// Returns '[ <13 empty items> ]', which is an array with 13 empty elements !
 
 /*
 * ************************************************************
-* isNumber()
+* Checks & Validation
 * */
+
+dd(typeof 148833);
+// Returns "number"
+
+dd(Number(148833).constructor.name);
+// Returns "Number"
+
+dd(new Number(148833));
+// Returns "[Number: 148833]"
+
+dd(new Number(148833).valueOf());
+// Returns "148833"
+
+dd(new Number(148833).constructor.name)
+// Returns "Number"
+
+// ************************************************************
+// Basic Integers
+
+dd(Number.isInteger(148833));
+// Returns 'true'
+
+dd(Number.isSafeInteger(148833));
+// Returns 'true'
+
+dd(Number.isFinite(148833));
+// Returns 'true'
+
+dd(Number.isNaN(148833));
+// Returns 'false'
+
+// ************************************************************
+// Decimal Numbers
+
+dd(Number.isInteger(38.742));
+// Returns 'false'
+
+dd(Number.isSafeInteger(38.742));
+// Returns 'false'
+
+dd(Number.isFinite(38.742));
+// Returns 'true'
+
+dd(Number.isNaN(38.742));
+// Returns 'false'
+
+// ************************************************************
+// Numbers as Strings
+
+dd(Number.isInteger('38.742'));
+// Returns 'false'
+
+dd(Number.isSafeInteger('38.742'));
+// Returns 'false'
+
+dd(Number.isFinite('38.742'));
+// Returns 'false'
+
+dd(Number.isNaN('38.742'));
+// Returns 'false'
+
+// ************************************************************
+// Number.isNaN . Not to be confused with the global isNaN()
+
+dd(Number.isNaN(undefined));
+// Returns 'false'
+
+dd(Number.isNaN(undefined * 2));
+// Returns 'true', impossible operation.
+
+dd(Number.isNaN('string123' * 2));
+// Returns 'true', mathematical operations on strings return NaN.
+
+dd(Number.isNaN(Math.sqrt(-2)));
+// Returns 'true', impossible number.
+
+
+dd(Number.isNaN(Number(undefined)));
+// Returns 'true', converting undefined to a number returns NaN
+
+
+dd(Number.isNaN(Number('3e5')));
+// Returns 'true',eap a5 printing
+
+
+dd(Number.isNaN(Number('my string')));
+// Returns 'true', converting a non-number string to a number returns NaN
+
+
+dd_enabled = false;
+
+
+dd(Number.isNaN(parseInt(undefined)));
+// Returns 'true', parsing undefined to int returns NaN.
+
+
+dd(Number.isNaN(Infinity * 0));
+// Returns 'true'
+
+// ************************************************************
+// Max Safe Integers
+
+dd(Math.pow(2, 53));
+// Returns '9007199254740992'
+
+
+dd(Number.isInteger(Math.pow(2, 53)));
+// Returns 'true'
+
+dd(Number.isSafeInteger(Math.pow(2, 53)));
+// Returns 'false', as 2^53-1 is the max safe number.
+
+dd(Number.isSafeInteger(Math.pow(2, 53) - 1));
+// Returns 'true'
+
+dd(Math.pow(2, 53) - 1 === Number.MAX_SAFE_INTEGER);
+// Returns 'true'
+
+dd(Number.isFinite(Math.pow(2, 53)));
+// Returns 'true'
+
+dd(Number.isNaN(Math.pow(2, 53)));
+// Returns 'false'
+
+// ************************************************************
+
+
 function isNumber(value) {
     return typeof value === 'number' && isFinite(value);
 }
--- vanilla-js/src/basics/data-type_string.js
+++ vanilla-js/src/basics/data-type_string.js
@@ -16,13 +16,15 @@ function this_line(stack_item_index = 2, show_filepath = true) {
     return message;
 }
 
+var dd_enabled = true;
 const dd = function (message, stack_item_index = 3, show_filepath = true) {
-
+    if (dd_enabled === false) {
+        return;
+    }
     console.log(message);
     console.log('|_ ' + this_line(stack_item_index, show_filepath));
 
 }
-
 
 /*
 * ************************************************************
@@ -43,27 +45,58 @@ let my_string_object = new String("This is my second string !!");
 * ************************************************************
 *
 * */
-dd(typeof my_string); // prints "string"
-dd(my_string.length); // prints "19"
+// prints "string"
+dd(typeof my_string);
 
-dd(my_string_object); // prints "[String: 'This is my second string !!']"
-dd(my_string_object.valueOf()); // prints "This is my second string !!"
-dd(my_string_object.constructor.name) // prints "String"
+// prints "19"
+dd(my_string.length);
+
+// prints "String"
+dd(my_string_object.constructor.name);
+
+
+// prints "[String: 'This is my second string !!']"
+dd(my_string_object);
+
+// prints "This is my second string !!"
+dd(my_string_object.valueOf());
+
+// prints "String"
+dd(my_string_object.constructor.name);
 
 /*
 * ************************************************************
 * Finding a string
 * */
-dd(my_string.indexOf('my')); // prints "8"
-dd(my_string.indexOf('is', 4)); // prints "5", as the search started from the 4th character.
-dd(my_string.indexOf('non-existing')); // prints "-1" when string is not found.
-dd(my_string.lastIndexOf(' ')); // 17
-dd(my_string.search(/my/g)); // prints '8', search done with regex.
-dd(my_string.startsWith('my', 8)); // prints 'true', after checking if 'my' is at the start of the string after applying the optional start offset.
-dd(my_string.endsWith('is', 7)); // prints 'true', after checking if 'is' is at the end of the string after applying the optional end offset.
-dd(my_string.includes('my')); // prints 'true'
-dd(my_string.match(/i[a-zA-Z]/ig)); // prints '[ 'is', 'is', 'in' ]'
-dd(my_string.repeat(2)); // prints the string repeated x times.
+// prints "8"
+dd(my_string.indexOf('my'));
+
+// prints "5", as the search started from the 4th character.
+dd(my_string.indexOf('is', 4));
+
+// prints "-1" when string is not found.
+dd(my_string.indexOf('non-existing'));
+
+// 17
+dd(my_string.lastIndexOf(' '));
+
+// prints '8', search done with regex.
+dd(my_string.search(/my/g));
+
+// prints 'true', after checking if 'my' is at the start of the string after applying the optional start offset.
+dd(my_string.startsWith('my', 8));
+
+// prints 'true', after checking if 'is' is at the end of the string after applying the optional end offset.
+dd(my_string.endsWith('is', 7));
+
+// prints 'true'
+dd(my_string.includes('my'));
+
+// prints '[ 'is', 'is', 'in' ]'
+dd(my_string.match(/i[a-zA-Z]/ig));
+
+// prints the string repeated x times.
+dd(my_string.repeat(2));
 
 /*
 * ************************************************************
@@ -71,31 +104,54 @@ dd(my_string.repeat(2)); // prints the string repeated x times.
 * */
 
 
-dd(my_string.slice(0, 7)); // prints 'This is', extracting characters 0-7 to a new variable.
-dd(my_string.slice(11)); // prints 'string !', extracting characters 11 onwards to a new variable.
-dd(my_string.slice(-4)); // prints 'ng !', extracting 4 characters from the end of the string to a new variable.
-dd(my_string.charAt(3)); // prints 's', extracting the 3rd characters from the end of the string to a new variable.
+// prints 'This is', extracting characters 0-7 to a new variable.
+dd(my_string.slice(0, 7));
+
+// prints 'string !', extracting characters 11 onwards to a new variable.
+dd(my_string.slice(11));
+
+// prints 'ng !', extracting 4 characters from the end of the string to a new variable.
+dd(my_string.slice(-4));
+
+// prints 's', extracting the 3rd characters from the end of the string to a new variable.
+dd(my_string.charAt(3));
 
 
 /*
 * substring does not support negative numbers & left as is for backward compatibility.
 * Use slice() for a more consistent outcome.
 * */
-dd(my_string.substring(0, 7)); // prints 'This is', extracting characters 0-7 to a new variable.
-dd(my_string.substring(11)); // prints 'string !', extracting characters 11 onwards to a new variable.
-dd(my_string.substring(-4)); // prints 'This is my string!', converting the negative "-4" to 0 then printing 0-string.length to a new variable.
+
+// prints 'This is', extracting characters 0-7 to a new variable.
+dd(my_string.substring(0, 7));
+
+// prints 'string !', extracting characters 11 onwards to a new variable.
+dd(my_string.substring(11));
+
+// prints 'This is my string!', converting the negative "-4" to 0 then printing 0-string.length to a new variable.
+dd(my_string.substring(-4));
 
 
-dd(my_string.substr(0, 7)); // prints 'This is', extracting 7 characters counting from 0 to a new variable.
-dd(my_string.substr(11)); // prints 'string !', extracting characters 11 onwards to a new variable.
-dd(my_string.substr(-4)); // prints 'ng !', extracting 4 characters from the end of the string to a new variable.
-dd(my_string.substr(3, 7)); // prints 's is my', extracting 7 characters counting from 3 to a new variable.
+// prints 'This is', extracting 7 characters counting from 0 to a new variable.
+dd(my_string.substr(0, 7));
 
-dd(my_string.replace('!', '??')); // prints 'This is my string ??', replacing the first occurrence of '!' with '??'
+// prints 'string !', extracting characters 11 onwards to a new variable.
+dd(my_string.substr(11));
 
-dd(my_string.replace('IS', 'was')); // prints 'This is my string !', the search value is case sensitive , so nothing was replaced!
+// prints 'ng !', extracting 4 characters from the end of the string to a new variable.
+dd(my_string.substr(-4));
 
-dd(my_string.replace(/IS/ig, 'huh')); // prints 'Thhuh huh my string !', replacing ALL occurrences of 'IS' with 'huh' using a regex. /i flag switched case-insensitive search while the /g flag switched on replace-all functionality .
+// prints 's is my', extracting 7 characters counting from 3 to a new variable.
+dd(my_string.substr(3, 7));
+
+// prints 'This is my string ??', replacing the first occurrence of '!' with '??'
+dd(my_string.replace('!', '??'));
+
+// prints 'This is my string !', the search value is case sensitive , so nothing was replaced!
+dd(my_string.replace('IS', 'was'));
+
+// prints 'Thhuh huh my string !', replacing ALL occurrences of 'IS' with 'huh' using a regex. /i flag switched case-insensitive search while the /g flag switched on replace-all functionality .
+dd(my_string.replace(/IS/ig, 'huh'));
 
 
 /*
@@ -103,8 +159,11 @@ dd(my_string.replace(/IS/ig, 'huh')); // prints 'Thhuh huh my string !', replaci
 * Converting a string to array
 * */
 
-dd(my_string.split('')); // prints an array of single characters.
-dd(my_string.split(' ')); // prints an array of words that were separated by a space.
+// prints an array of single characters.
+dd(my_string.split(''));
+
+// prints an array of words that were separated by a space.
+dd(my_string.split(' '));
 
 
 /*
@@ -116,9 +175,9 @@ dd(my_string.split(' ')); // prints an array of words that were separated by a s
 
 /*
 * ************************************************************
-* isString()
+* Validation
 * */
 
-function isString (value) {
+function isString(value) {
     return typeof value === 'string' || value instanceof String;
 }
