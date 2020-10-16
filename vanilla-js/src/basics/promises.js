b5b9dc8929a8ac801d6c0fbe8152f35d928e1786
Michael Habib <1233256+MichaelHabib@users.noreply.github.com>
2020-10-16 19:32:35 +1100

    M  vanilla-js/readme.adoc M  vanilla-js/src/basics/promises.js

--- vanilla-js/src/basics/promises.js
+++ vanilla-js/src/basics/promises.js
@@ -5,27 +5,61 @@
 * */
 
 function this_line(stack_item_index = 2, show_filepath = true) {
-    const e = new Error();
-    const regex = /\((.*):(\d+):(\d+)\)$/
-    const match = regex.exec(e.stack.split("\n")[stack_item_index]);
-    let message = ''
-    if (show_filepath) {
-        message += match[1] + ' : ';
+    // const e = new Error();
+    try {
+        throw Error()
+    } catch (err) {
+        var e = err;
     }
-    message += '' + match[2] + ":" + match[3];
+    const split_error_stack = e.stack.split("\n");
+    const find_index_of_line_with_dd = split_error_stack.findIndex((item, index) => {
+        return item.indexOf('at dd') >= 0;
+    });
+    const correct_filepath_error_stack_item_index = find_index_of_line_with_dd + 1;
+
+
+    let message = ''
+
+    message += split_error_stack[correct_filepath_error_stack_item_index];
     return message;
+
+    // broken regex due to error inside Promise functions returning a different structure for 'e.stack'
+    // const regex = /\((.*):(\d+):(\d+)\)$/
+    // const match = regex.exec(split_error_stack[correct_filepath_error_stack_item_index]);
+    // if (show_filepath) {
+    //     message += match[1] + ' : ';
+    // }
+    // message += '' + match[2] + ":" + match[3];
+    // return message;
 }
 
 var dd_enabled = true;
-const dd = function (message, stack_item_index = 3, show_filepath = true) {
-    if (dd_enabled === false) {
-        return;
-    }
-    console.log(message);
-    console.log('|_ ' + this_line(stack_item_index, show_filepath));
+// const dd = function (message, stack_item_index = 2, show_filepath = true) {
+//     if (dd_enabled === false) {
+//         return;
+//     }
+//     console.log(message);
+//     console.log('|_ ' + this_line(stack_item_index, show_filepath));
+//
+// }
 
+function easy_dd() {
+    this.stack_item_index = 3;
+    this.show_filepath = true;
+
+    this.dd = (message) => {
+        console.log(message);
+        console.log('|_ ' + this_line(this.stack_item_index, this.show_filepath));
+    }
+    this.set_stack_item_index = (i) => {
+        this.stack_item_index = i;
+    };
+    return this;
 }
 
+let custom_easy_dd = new easy_dd();
+custom_easy_dd.set_stack_item_index(3)
+const dd = custom_easy_dd.dd;
 
 
 /*
@@ -34,4 +68,14 @@ const dd = function (message, stack_item_index = 3, show_filepath = true) {
 * ************************************************************
  * */
 
-let my_promise = new Promise();
+let my_promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done"), 1000);
+
+});
+my_promise
+    .then((result) => {
+        dd(result);
+    })
+    .catch((e) => {
+        dd(e);
+    });
